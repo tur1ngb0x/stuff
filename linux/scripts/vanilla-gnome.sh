15a0e1030036e4a3fdc54f8f7d3a2f069cefe2b2
tur1ngb0x <25666761+tur1ngb0x@users.noreply.github.com>
2021-07-11 19:02:44 +0530

    added script for vanilla gnome experience

---
+++ linux/scripts/vanilla-gnome.sh
@@ -0,0 +1,15 @@
+
+sudo apt install -y gnome-session
+sudo apt install -y adwaita-icon-theme-full
+sudo apt install -y fonts-cantarell
+sudo apt install -y gnome-themes-extra
+
+sudo apt install -y dconf-editor
+sudo apt install -y gnome-tweaks
+
+sudo apt install -y vanilla-gnome-default-settings
+#sudo apt install -y vanilla-gnome-desktop
+
+sudo apt install fonts-liberation
+sudo apt install fonts-liberation2
+sudo apt-get install fonts-noto*
