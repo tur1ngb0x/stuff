b1394025f2effc263e02d9bd7a22c470763f6d26
tur1ngb0x <25666761+tur1ngb0x@users.noreply.github.com>
2021-07-15 16:06:50 +0530

    vanilla gnome install script

--- linux/scripts/vanilla-gnome.sh
+++ linux/scripts/vanilla-gnome.sh
@@ -1,15 +1,17 @@
+#!/usr/bin/env bash
 
+# Vanilla GNOME Packages
 sudo apt install -y gnome-session
 sudo apt install -y adwaita-icon-theme-full
 sudo apt install -y fonts-cantarell
 sudo apt install -y gnome-themes-extra
+sudo apt install -y vanilla-gnome-default-settings
 
+# GNOME Tweaker
 sudo apt install -y dconf-editor
 sudo apt install -y gnome-tweaks
 
-sudo apt install -y vanilla-gnome-default-settings
-#sudo apt install -y vanilla-gnome-desktop
-
+# Extra Font Support
 sudo apt install fonts-liberation
 sudo apt install fonts-liberation2
 sudo apt-get install fonts-noto*
